f6562d29bceb4ea622447cc30d9d6ca43d528f64
Hector Hernandez <hhernandez@ryze-tech.com>
2021-12-02 04:04:28 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-url=$(curl -s https://api.github.com/repos/hhernandez92/c3-microservice-release$
+url=$(curl -s https://api.github.com/repos/hhernandez92/c3-microservice-release/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \")
 
-baseDirectory="/tmp/fusion/"
-fileName=$(basename $url)
-filePath=${baseDirectory}${fileName}
+echo $url
 
-wget -q ${url} -P "${baseDirectory}"
+#baseDirectory="/tmp/fusion/"
+#fileName=$(basename $url)
+#filePath=${baseDirectory}${fileName}
 
-apt install "${filePath}"
-rm "${filePath}"
+#wget -q ${url} -P "${baseDirectory}"
+
+#apt install "${filePath}"
+#rm "${filePath}"
 
 exit 0
