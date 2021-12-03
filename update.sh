a5ecf88521102f04a0dea11a011e7c400def0ecb
Hector Hernandez <hhernandez@ryze-tech.com>
2021-12-03 16:18:17 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-url=$(curl -s https://api.github.com/repos/hhernandez92/c3-microservice-release/releases/latest \
+url=$(curl -s https://api.github.com/repos/Modern-Robotics/c3-microservice-release/releases/latest \
 | grep "browser_download_url.*deb" \
 | cut -d : -f 2,3 \
 | tr -d \")
